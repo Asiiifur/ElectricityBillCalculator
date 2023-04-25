d2cc23697c03fc3adff7a26d6763ddf50b438efe
ASIF <ASIF@ASIF>
2023-04-25 12:09:01 +0600

    Add project files.

---
+++ ElectricityBillCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ElectricityBillCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                Console.WriteLine("Input Appliance power Watt :");
+                double Watt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input Uses Time (H)/Day:");
+                double Time = Convert.ToInt32(Console.ReadLine());
+
+
+                var unit = ((Watt * Time) / 1000);
+                Console.WriteLine("Total Per Unit Par day");
+                Console.WriteLine(unit);
+
+                Console.WriteLine("Input Total Days In this Month:");
+                double MonthDays = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Input Per Unit Price:");
+                double unitPrice = Convert.ToInt32(Console.ReadLine());
+
+                var totalUnit = (unit * MonthDays);
+                var TotalBill = (totalUnit * unitPrice);
+
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Total Bill :");
+                Console.WriteLine(TotalBill);
+
+
+                Console.Read();
+
+
+        
+         }
+    }
+}
